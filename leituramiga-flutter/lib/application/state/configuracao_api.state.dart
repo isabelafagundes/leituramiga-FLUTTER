a6cfef34581182b083613b4765c3c322c48cae31
isabelafagundes <isabelahidalgo.2004@gmail.com>
2024-11-23 18:52:54 -0300

    Testando flutter build web

--- leituramiga-flutter/lib/application/state/configuracao_api.state.dart
+++ leituramiga-flutter/lib/application/state/configuracao_api.state.dart
@@ -1,9 +1,10 @@
 import 'dart:io';
 
 abstract mixin class ConfiguracaoApiState {
-  static String _ip = 'localhost';
-  static const String _porta = '8080';
-  static const String _protocolo = 'http';
+  static String _ip = '57b7-2804-1b3-ac03-aa-94e3-9141-e0a0-39b8.ngrok-free.app';
+  static const String _porta = '';
+  static const String _protocolo = 'https';
 
-  String get host => '$_protocolo://$_ip:$_porta/api';
+  String get host => _porta.isEmpty ? "$_protocolo://$_ip/api" : '$_protocolo://$_ip:$_porta/api';
 }
+
